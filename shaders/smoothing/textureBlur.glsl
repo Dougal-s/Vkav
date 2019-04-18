7aa4a61274980594292ea976286065f4517e32a0
Dougal Stewart <dougal.koji@gmail.com>
2019-04-19 00:18:26 +0900

    Changed blur type

--- shaders/smoothing/textureBlur.glsl
+++ shaders/smoothing/textureBlur.glsl
@@ -1,16 +1,18 @@
 
-// gaussian blur
+// bokeh blur
 vec4 blurredTexture(in sampler2D image, in vec2 position, in float volume) {
 	vec4 pixel = vec4(0.f, 0.f, 0.f, 0.f);
 
-	float radius = volume;
+	const float radius = volume;
+	const float stepSize = min(1.f/textureSize(image, 0).x, 1.f/textureSize(image, 0).y);
 
 	float sum = 0;
-	for (float y = position.y-radius; y <= position.y+radius; y += 1.f/textureSize(image, 0).y) {
-		for (float x = position.x-radius; x <= position.x+radius; x += 1.f/textureSize(image, 0).x) {
-			float weight = exp( ((x-position.x)*(x-position.x)/(2*volume)) * ((y-position.y)*(y-position.y)/(2*volume)) );
-			pixel += texture(image, vec2(x, y))*weight;
-			sum += weight;
+	for (float y = position.y-radius; y <= position.y+radius; y += stepSize) {
+		for (float x = position.x-radius; x <= position.x+radius; x += stepSize) {
+			if (distance(position, vec2(x,y)) <= distance(position, vec2(volume, volume))) {
+				pixel += texture(image, vec2(x, y));
+				++sum;
+			}
 		}
 	}
 
