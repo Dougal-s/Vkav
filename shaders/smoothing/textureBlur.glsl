62b9a33a42cf0a903b4dc896980742df9c7fba4d
Dougal Stewart <dougal.koji@gmail.com>
2019-04-19 13:44:46 +0900

    Fixed image blurring

--- shaders/smoothing/textureBlur.glsl
+++ shaders/smoothing/textureBlur.glsl
@@ -1,17 +1,28 @@
 
 // bokeh blur
-vec4 blurredTexture(in sampler2D image, in vec2 position, in float volume) {
+vec4 blurredTexture(in sampler2D image, in vec2 position, float volume) {
 	vec4 pixel = vec4(0.f, 0.f, 0.f, 0.f);
 
+	const float stepSize = 1.f/max(textureSize(image, 0).x, textureSize(image, 0).y);
 	const float radius = volume;
-	const float stepSize = min(1.f/textureSize(image, 0).x, 1.f/textureSize(image, 0).y);
 
 	float sum = 0;
-	for (float y = position.y-radius; y <= position.y+radius; y += stepSize) {
-		for (float x = position.x-radius; x <= position.x+radius; x += stepSize) {
-			if (distance(position, vec2(x,y)) <= distance(position, vec2(volume, volume))) {
-				pixel += texture(image, vec2(x, y));
-				++sum;
+
+	// start at centre pixel and go outwards.
+	for (float yOff = 0; yOff <= radius; yOff += stepSize) {
+		float y1 = position.y+yOff;
+		float y2 = position.y-yOff;
+		for (float xOff = 0; xOff <= radius; xOff += stepSize) {
+			float x1 = position.x+xOff;
+			float x2 = position.x-xOff;
+
+			if (distance(position, vec2(x1, y1)) <= volume) {
+				pixel += texture(image, vec2(x1, y1));
+				pixel += texture(image, vec2(x1, y2));
+				pixel += texture(image, vec2(x2, y1));
+				pixel += texture(image, vec2(x2, y2));
+
+				sum += 4;
 			}
 		}
 	}
