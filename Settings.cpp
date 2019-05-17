92639c2f98e87cc5d90c1893b9acd6d38c09674e
Dougal Stewart <dougal.koji@gmail.com>
2019-05-17 14:28:54 +0900

    reordered readCmdLineArgs()

--- Settings.cpp
+++ Settings.cpp
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 
 #include "Settings.hpp"
@@ -64,64 +65,58 @@ std::unordered_map<char, std::string> readCmdLineArgs(int argc,
                                                       const char* argv[]) {
 	std::unordered_map<char, std::string> arguments(argc - 1);
 	for (int i = 1; i < argc; ++i) {
-		std::string argValue;
-		std::string argName;
-		for (size_t size = 0; argv[i][size] != 0; ++size) {
-			if (argv[i][size] == '=') argValue = argv[i] + size + 1;
-			if (argValue.empty()) argName.push_back(argv[i][size]);
-		}
-
-		if (argv[i][0] == '-') {
-			if (argv[i][1] == '-') {
-				char charKey = 0;
-
-				if (argName == "--verbose") {
-					charKey = 'v';
-				} else if (argName == "--sink-name") {
-					charKey = 's';
-				} else if (argName == "--device") {
-					charKey = 'd';
-				} else if (argName == "--config-file") {
-					charKey = 'c';
-				} else if (argName == "--amplitude") {
-					charKey = 'a';
-				} else if (argName == "--help") {
-					charKey = 'h';
-				} else if (argName == "--version") {
-					charKey = 'V';
-				}
-
-				if (charKey == 0) {
-					throw std::invalid_argument(
-					    __FILE__ ": Invalid command line argument!");
-				}
-
-				arguments.insert(std::make_pair(charKey, argValue));
-
-			} else {
-				if (argv[i][1] != 'v' && argv[i][1] != 's' &&
-				    argv[i][1] != 'd' && argv[i][1] != 'c' &&
-				    argv[i][1] != 'a' && argv[i][1] != 'h' &&
-				    argv[i][1] != 'V') {
-					throw std::invalid_argument(
-					    __FILE__ ": Invalid command line argument!");
-				}
-
-				char key = argv[i][1];
-				const char* value;
-				if (argv[i][2] == '\0' && i + 1 < argc &&
-				    argv[i + 1][0] != '-') {
-					value = argv[i + 1];
-					++i;
-				} else {
-					value = argv[i] + 2;
-				}
-
-				arguments.insert(std::make_pair(key, value));
-			}
-		} else {
+		if (argv[i][0] != '-')
 			throw std::invalid_argument(__FILE__
 			                            ": Invalid command line argument!");
+
+		std::string argValue;
+		std::string argName = argv[i];
+
+		for (size_t size = 0; argv[i][size]; ++size) {
+			if (argv[i][size] == '=') {
+				argName.resize(size);
+				argValue = argv[i] + size + 1;
+				break;
+			}
+		}
+
+		if (argv[i][1] == '-') {
+			char charKey = 0;
+
+			static const std::unordered_map<std::string, char>
+			    cmdLineArgKeyMap = {
+			        {"--verbose", 'v'},   {"--sink-name", 's'},
+			        {"--device", 'd'},    {"--config-file", 'c'},
+			        {"--amplitude", 'a'}, {"--help", 'h'},
+			        {"--version", 'V'}};
+
+			if (const auto it = cmdLineArgKeyMap.find(argName);
+			    it != cmdLineArgKeyMap.end()) {
+				charKey = it->second;
+			}
+
+			if (charKey == 0)
+				throw std::invalid_argument(__FILE__
+				                            ": Invalid command line argument!");
+
+			arguments.insert(std::make_pair(charKey, argValue));
+
+		} else {
+			static const std::unordered_set<char> validCmdLineFlags = {
+			    'v', 's', 'd', 'c', 'a', 'h', 'V'};
+			if (validCmdLineFlags.find(argv[i][1]) == validCmdLineFlags.end())
+				throw std::invalid_argument(__FILE__
+				                            ": Invalid command line argument!");
+
+			char key = argv[i][1];
+			const char* value;
+
+			if (!argv[i][2] && i + 1 < argc && argv[i + 1][0] != '-')
+				value = argv[++i];
+			else
+				value = argv[i] + 2;
+
+			arguments.insert(std::make_pair(key, value));
 		}
 	}
 	return arguments;
