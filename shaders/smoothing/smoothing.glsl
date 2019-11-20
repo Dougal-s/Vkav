391f81e1638b8265e1192b489a88e925eb485612
Dougal Stewart <dougal.koji@gmail.com>
2019-11-20 22:24:11 +0900

    fixed exponential address calculation in smoothing.glsl

--- shaders/smoothing/smoothing.glsl
+++ shaders/smoothing/smoothing.glsl
@@ -1,18 +1,22 @@
 // simulates address mode VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT
+float wrapIndex(float index) {
+	return 1.f - abs(mod(abs(index), 2.f)-1.f);
+}
+
 float texture(in samplerBuffer s, in float index) {
-	return texelFetch(s, int(( 1.f - abs(mod(abs(index), 2.f)-1.f) )*audioSize)).r;
+	return texelFetch(s, int(wrapIndex(index)*audioSize)).r;
+}
+
+float textureExp(in samplerBuffer s, in float index) {
+	return texelFetch(s, int(exp2(wrapIndex(index))*audioSize-audioSize)).r;
 }
 
 float kernelSmoothTexture(in samplerBuffer s, in float index) {
-	// simulates VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT to clamp input between 0 and 1
-	index = 1.f - abs(mod(abs(index), 2.f)-1.f);
+	// clamp input between 0 and 1
+	index = wrapIndex(index);
 
 	if (smoothingLevel == 0.f)
 		return texture(s, index);
-
-	#ifdef exponential
-		index = exp2(index)-1.f;
-	#endif
 
 	const float pi = 3.14159265359;
 	float val = 0.f;
@@ -22,23 +26,23 @@ float kernelSmoothTexture(in samplerBuffer s, in float index) {
 	for (float i = index-radius; i < index+radius; i += 1.f/audioSize) {
 		const float distance = audioSize*(index - i);
 		const float weight = exp(-distance*distance*smoothingFactor);
-		val += texture(s, i)*weight;
+		#ifdef exponential
+			val += textureExp(s, i)*weight;
+		#else
+			val += texture(s, i)*weight;
+		#endif
 	}
-	val /= sqrt(pi/smoothingFactor);
+	val *= sqrt(smoothingFactor/pi);
 
 	return val;
 }
 
 float mcatSmoothTexture(in samplerBuffer s, in float index) {
-	// simulates VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT to clamp input between 0 and 1
-	index = 1.f - abs(mod(abs(index), 2.f)-1.f);
+	// clamp input between 0 and 1
+	index = wrapIndex(index);
 
 	if (smoothingLevel == 0.f)
 		return texture(s, index);
-
-	#ifdef exponential
-		index = exp2(index)-1.f;
-	#endif
 
 	const float pi = 3.14159265359;
 	float val = 0.f;
@@ -47,7 +51,12 @@ float mcatSmoothTexture(in samplerBuffer s, in float index) {
 	const float radius = log(30.f)/(log(smoothingFactor)*audioSize);
 	for (float i = index-radius; i < index+radius; i += 1.f/audioSize) {
 		const float distance = audioSize*abs(i - index);
-		val = max(texture(s, i) * pow(smoothingFactor, -distance), val);
+
+		#ifdef exponential
+			val = max(textureExp(s, i) * pow(smoothingFactor, -distance), val);
+		#else
+			val = max(texture(s, i) * pow(smoothingFactor, -distance), val);
+		#endif
 	}
 	val *= 0.3;
 
