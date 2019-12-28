28f038186af330c371c5069671cb7eb918fd3be4
Dougal Stewart <dougal.koji@gmail.com>
2019-12-29 00:18:54 +0900

    created method for easily building .deb packages

---
+++ deb-package/README.md
@@ -0,0 +1,3 @@
+# Building a deb package
+
+After compiling Vkav, simply execute `build.sh` to build a deb package. After installation, Vkav can be used normally.
---
+++ deb-package/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+mkdir "vkav_0.1-1/usr"
+mkdir "vkav_0.1-1/usr/bin"
+cp "../build/Vkav" "vkav_0.1-1/usr/bin"
+
+mkdir "vkav_0.1-1/etc"
+mkdir "vkav_0.1-1/etc/Vkav"
+cp "../src/config" "vkav_0.1-1/etc/Vkav"
+cp -r "../src/shaders" "vkav_0.1-1/etc/Vkav"
+
+mkdir "vkav_0.1-1/usr/share"
+mkdir "vkav_0.1-1/usr/share/icons"
+mkdir "vkav_0.1-1/usr/share/icons/hicolor"
+
+mkdir "vkav_0.1-1/usr/share/icons/hicolor/48x48"
+mkdir "vkav_0.1-1/usr/share/icons/hicolor/48x48/apps"
+cp "../Vkav.png" "vkav_0.1-1/usr/share/icons/hicolor/48x48/apps"
+
+mkdir "vkav_0.1-1/usr/share/icons/hicolor/scalable"
+mkdir "vkav_0.1-1/usr/share/icons/hicolor/scalable/apps"
+cp "../Vkav.svg" "vkav_0.1-1/usr/share/icons/hicolor/scalable/apps"
+
+mkdir "vkav_0.1-1/usr/share/applications"
+cp "../Vkav.desktop" "vkav_0.1-1/usr/share/applications"
+
+dpkg-deb --build vkav_0.1-1
