61a75242378b77cbaf9ff433fe4c6b5fbda3f0ca
Dougal Stewart <dougal.koji@gmail.com>
2020-02-01 03:37:31 +0900

    updated deb package to v0.3.0

--- deb-package/build.sh
+++ deb-package/build.sh
@@ -2,28 +2,28 @@
 
 cd "$(dirname "$0")"
 
-mkdir "vkav_0.2-1/usr"
-mkdir "vkav_0.2-1/usr/bin"
-cp "../build/vkav" "vkav_0.2-1/usr/bin"
+mkdir "vkav_0.3-1/usr"
+mkdir "vkav_0.3-1/usr/bin"
+cp "../build/vkav" "vkav_0.3-1/usr/bin"
 
-mkdir "vkav_0.2-1/etc"
-mkdir "vkav_0.2-1/etc/vkav"
-cp "../src/config" "vkav_0.2-1/etc/vkav"
-cp -r "../src/shaders" "vkav_0.2-1/etc/vkav"
+mkdir "vkav_0.3-1/etc"
+mkdir "vkav_0.3-1/etc/vkav"
+cp "../src/config" "vkav_0.3-1/etc/vkav"
+cp -r "../src/shaders" "vkav_0.3-1/etc/vkav"
 
-mkdir "vkav_0.2-1/usr/share"
-mkdir "vkav_0.2-1/usr/share/icons"
-mkdir "vkav_0.2-1/usr/share/icons/hicolor"
+mkdir "vkav_0.3-1/usr/share"
+mkdir "vkav_0.3-1/usr/share/icons"
+mkdir "vkav_0.3-1/usr/share/icons/hicolor"
 
-mkdir "vkav_0.2-1/usr/share/icons/hicolor/48x48"
-mkdir "vkav_0.2-1/usr/share/icons/hicolor/48x48/apps"
-cp "../vkav.png" "vkav_0.2-1/usr/share/icons/hicolor/48x48/apps"
+mkdir "vkav_0.3-1/usr/share/icons/hicolor/48x48"
+mkdir "vkav_0.3-1/usr/share/icons/hicolor/48x48/apps"
+cp "../vkav.png" "vkav_0.3-1/usr/share/icons/hicolor/48x48/apps"
 
-mkdir "vkav_0.2-1/usr/share/icons/hicolor/scalable"
-mkdir "vkav_0.2-1/usr/share/icons/hicolor/scalable/apps"
-cp "../vkav.svg" "vkav_0.2-1/usr/share/icons/hicolor/scalable/apps"
+mkdir "vkav_0.3-1/usr/share/icons/hicolor/scalable"
+mkdir "vkav_0.3-1/usr/share/icons/hicolor/scalable/apps"
+cp "../vkav.svg" "vkav_0.3-1/usr/share/icons/hicolor/scalable/apps"
 
-mkdir "vkav_0.2-1/usr/share/applications"
-cp "../vkav.desktop" "vkav_0.2-1/usr/share/applications"
+mkdir "vkav_0.3-1/usr/share/applications"
+cp "../vkav.desktop" "vkav_0.3-1/usr/share/applications"
 
-dpkg-deb --build vkav_0.2-1
+dpkg-deb --build vkav_0.3-1
