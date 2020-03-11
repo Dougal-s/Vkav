8b5079dd4564adbc14f784e83eb8617f7c29f4e0
Dougal Stewart <dougal.koji@gmail.com>
2020-03-11 17:46:59 +0900

    Created MacOS directory for building macos .app bundles

---
+++ MacOS/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "${0%/*}"
+
+# start.sh
+mkdir ./Vkav.app/Contents/Macos
+echo "#!/bin/bash" > ./Vkav.app/Contents/Macos/start.sh
+echo "cd \"\${0%/*}\"" >> ./Vkav.app/Contents/Macos/start.sh
+echo "VK_ICD_FILENAMES=\"$1/macOS/etc/vulkan/icd.d/MoltenVK_icd.json\" ./vkav \"\$@\"" >> ./Vkav.app/Contents/Macos/start.sh
+
+# vkav
+cp ../build/vkav ./Vkav.app/Contents/Macos
+
+# config
+mkdir ./Vkav.app/Contents/Resources/vkav
+cp ../src/config ./Vkav.app/Contents/Resources/vkav
+
+# modules
+cp ../src/modules -r ./Vkav.app/Contents/Resources/vkav
